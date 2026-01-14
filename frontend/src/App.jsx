decae6953c287d9b4ee4353d73e046cbc2e84928
RITIK SINGH <111556793+Ritik10x@users.noreply.github.com>
2026-01-14 19:59:26 +0530

    FrontEnd updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Contact from "./Components/Contact.jsx"
 import Experience from "./Components/Expiernce.jsx"
 
 import ProfilePicture from "./Components/ProfilePhoto.jsx"
-import Project from "./Components/Projects.jsx"
+import Project2 from "./Components/Projects2.jsx"
 import Skills from "./Components/Skills.jsx"
 import HeroSection from "./Components/HeroSection.jsx"
 
@@ -19,7 +19,7 @@ function App(){
       
       <section id="home"><HeroSection/></section>
       <section id="Experience"><Experience/></section>
-      <section id="Project"><Project/></section>
+      <section id="Project"><Project2/></section>
       <section id="Skills"><Skills/></section>
       <section id="Contact"><Contact/></section>
       </main>
